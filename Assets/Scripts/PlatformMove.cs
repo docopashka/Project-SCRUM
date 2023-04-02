3aa0bf15cc2f258216a1bb8af12cd7c5183e0acb
Aloxa123 <smit4276@mail.ru>
2023-04-02 20:01:58 +0700

    level complited

--- Assets/Scripts/PlatformMove.cs
+++ Assets/Scripts/PlatformMove.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlatformMove : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform pos1, pos2;
+    public float speed = 1f;
+    public Transform startPos;
+
+    Vector3 nextPos;
+
     void Start()
     {
-        
+       
+        nextPos = startPos.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+
+        Debug.Log(pos1.position);
+
+        if(transform.position == pos1.position)
+        {
+            nextPos = pos2.position;
+        }
+        if(transform.position == pos2.position)
+        {
+            nextPos = pos1.position;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(pos1.position, pos2.position);
     }
 }
